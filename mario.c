bbf9e8cb350a2d86a71ce40ea999d22689a4dc2a
Selmayldz <117008166+Selmayldz@users.noreply.github.com>
2022-12-07 12:27:26 +0300

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void piramit(int a)
+{
+    int i,j;
+
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=(a-i);j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("#");
+        }
+        for(j=1;j<=2;j++)
+        {
+            printf(" ");
+        }
+         for(j=1;j<=i;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int a;
+    printf("$ ./mario\n");
+   loop: printf("height:");
+    scanf("%d",&a);
+
+    if(a>0 && a<=8)
+        piramit(a);
+    else
+        goto loop;
+
+    return 0;
+}
